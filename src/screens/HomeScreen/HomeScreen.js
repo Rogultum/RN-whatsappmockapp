1268c6da9153c691c4066d2eba965aedcc39b502
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-08-25 17:47:39 +0300

    add 'homescreen' screen+style

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { FlatList, Pressable, View, Text } from "react-native";
+import Ionicon from "react-native-vector-icons/Ionicons";
+import styles from "./HomeScreen.style";
+import UserData from "../../components/data/UserData.json";
+import ChatList from "../../components/ChatList/ChatList";
+
+function HomeScreen({ navigation }) {
+  const [list] = useState(UserData);
+
+  const renderChat = ({ item }) => <ChatList user={item} />;
+  const renderSeperator = () => <View style={styles.seperator} />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inner_container}>
+        <Ionicon.Button
+          name="camera"
+          color="#CEE5D0"
+          backgroundColor="transparent"
+          size={25}
+        />
+        <Text style={styles.text_style}>CHATS</Text>
+        <Text style={styles.text_style}>STATUS</Text>
+        <Text style={styles.text_style}>CALLS</Text>
+      </View>
+
+      <Pressable onPress={() => navigation.navigate("Chat")}>
+        <FlatList
+          style={styles.header}
+          data={list}
+          keyExtractor={(item) => item.id}
+          renderItem={renderChat}
+          ItemSeparatorComponent={renderSeperator}
+        />
+      </Pressable>
+    </View>
+  );
+}
+
+export default HomeScreen;
---
+++ src/screens/HomeScreen/HomeScreen.style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {},
+  header: {},
+  inner_container: {
+    backgroundColor: "#075E54",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  text_style: {
+    color: "white",
+    fontWeight: "bold",
+    marginRight: 24,
+  },
+  list_container: {
+    flexDirection: "row",
+  },
+  seperator: {
+    borderWidth: 0.55,
+    borderColor: "#e0e0e0",
+  },
+});
