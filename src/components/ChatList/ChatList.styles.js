dbad18f484c06fcd478b644b1b967a1c71d142ae
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-08-25 17:48:28 +0300

    add chatlist component+styles

---
+++ src/components/ChatList/ChatList.styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  img_style: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+  },
+  inner_container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingLeft: 10,
+  },
+  message_container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  name_style: {
+    fontWeight: "bold",
+  },
+});
